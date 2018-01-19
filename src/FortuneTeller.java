a7db6a540c4ce69656ceb80eedb1976637ecb4c8
Kasey <KaseyColian@gmail.com>
2018-01-19 12:08:39 -0500

    Finished Siblings

--- src/FortuneTeller.java
+++ src/FortuneTeller.java
@@ -13,22 +13,19 @@ public class FortuneTeller {
 		int age = input.nextInt();
 		System.out.println("Please enter your birth month in numeric form.");
 		int birthMonth = input.nextInt();
-		input.nextLine();		
+		input.nextLine();
 
-		System.out.println("Please enter your favorite color from ROYGBIV using the full word.  Type 'help' for examples.");
-		
+		System.out.println(
+				"Please enter your favorite color from ROYGBIV using the full word.  Type 'help' for examples.");
+
 		String favoriteColor = input.nextLine();
-		
+
 		if (favoriteColor.equalsIgnoreCase("help")) {
-			System.out.println("R is Red\nO is Orange\nY is Yellow\nG is Green\nB is Blue\nI is Indigo\nV is Violet\nNow please enter your favorite color.");
+			System.out.println(
+					"R is Red\nO is Orange\nY is Yellow\nG is Green\nB is Blue\nI is Indigo\nV is Violet\nNow please enter your favorite color.");
 		}
 		favoriteColor = input.nextLine();
-		
-		
-		
-		
-		
-
+System.out.println("How many siblings do you have?");
+int siblings = input.nextInt();
 	}
-	}
-
+}
