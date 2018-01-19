fff4c37f60c8db85bb5a52622f949739ca3dea92
Kasey <KaseyColian@gmail.com>
2018-01-19 11:30:56 -0500

    Broken Birthday Month for Correct Date, Program Hangs

--- src/FortuneTeller.java
+++ src/FortuneTeller.java
@@ -3,27 +3,30 @@ import java.util.Scanner;
 public class FortuneTeller {
 
 	public static void main(String[] args) {
-		Scanner input = new Scanner (System.in);
-		
+		Scanner input = new Scanner(System.in);
+
 		System.out.println("Please enter your first name.");
 		String firstName = input.nextLine();
 		System.out.println("Please enter your last name.");
 		String lastName = input.nextLine();
-		System.out.println ("Please enter your age.");
+		System.out.println("Please enter your age.");
 		int age = input.nextInt();
 		System.out.println("Please enter your birth month in numeric form.");
 		int birthMonth = input.nextInt();
-		if (birthMonth>12)
+		if (birthMonth > 12 | birthMonth <= 0)
+		{
 			System.out.println("Please enter a month from 1-12.");
-		int birthMonthWrong = input.nextInt();
-	
+			input.next();
+
+		}
+		else if (birthMonth < 12 && birthMonth > 0); input.next();
+
 		System.out.println("Please enter your favorite color from ROYGBIV.  Type 'help' for examples.");
-		
-		
-			
-		
-		
-				
+		String color = input.nextLine();
+		if (color.contains("help")) {
+			System.out.println("R is Red" + "O is Orange" + "Y is Yellow");
+		}
+
 	}
 
 }
