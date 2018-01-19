308ead44403064878f70e9e8bebc68afbb2a33a4
Kasey <KaseyColian@gmail.com>
2018-01-19 12:03:30 -0500

    Finished Birth Month Section

--- src/FortuneTeller.java
+++ src/FortuneTeller.java
@@ -5,21 +5,27 @@ public class FortuneTeller {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 
-		System.out.println("Please enter your first name.");
-		String firstName = input.nextLine();
-		System.out.println("Please enter your last name.");
-		String lastName = input.nextLine();
-		System.out.println("Please enter your age.");
-		int age = input.nextInt();
-		System.out.println("Please enter your birth month in numeric form.");
-		int birthMonth = input.nextInt();
+//		System.out.println("Please enter your first name.");
+//		String firstName = input.nextLine();
+//		System.out.println("Please enter your last name.");
+//		String lastName = input.nextLine();
+//		System.out.println("Please enter your age.");
+//		int age = input.nextInt();
+//		System.out.println("Please enter your birth month in numeric form.");
+//		int birthMonth = input.nextInt();
 
-		System.out.println("Please enter your favorite color from ROYGBIV.  Type 'help' for examples.");
-		String color = input.nextLine();
-		if (color.contains("help")) {
-			System.out.println("R is Red" + "O is Orange" + "Y is Yellow");
+		System.out.println("Please enter your favorite color from ROYGBIV using the full word.  Type 'help' for examples.");
+		
+		String favoriteColor = input.nextLine();
+		
+		if (favoriteColor.equalsIgnoreCase("help")) {
+			System.out.println("R is Red\nO is Orange\nY is Yellow\nG is Green\nB is Blue\nI is Indigo\nV is Violet\nNow please enter your favorite color.");
 		}
+		favoriteColor = input.nextLine();
+		
+		
+		
 
 	}
+	}
 
-}
