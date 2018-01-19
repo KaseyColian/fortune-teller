f75e09979f2a463a105c52663ba50c58891f899b
Kasey <KaseyColian@gmail.com>
2018-01-19 10:16:25 -0500

    Added First Name Line

--- src/FortuneTeller.java
+++ src/FortuneTeller.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
 
 public class FortuneTeller {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner (System.in);
+		
+		System.out.println("Please enter your first name.");
+		String firstName = input.nextLine();
+		
 	}
 
 }
